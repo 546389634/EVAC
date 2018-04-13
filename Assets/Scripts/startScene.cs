87b429b2661d2b285aa3680459f792a0f4b8cd59
546389634 <546389634@qq.com>
2018-04-14 02:27:14 +0800

    突出重围菜单&对战模式

---
+++ Assets/Scripts/startScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startScene : MonoBehaviour {
+    public Button Btn_startGame;
+    public Button Btn_exitGame;
+    // Use this for initialization
+    void Start () {
+        Btn_startGame.onClick.AddListener(playGame);
+        Btn_exitGame.onClick.AddListener(exitGame);
+    
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    public void playGame() {
+        Application.LoadLevel("Start");
+
+    }
+    public void exitGame() {
+        Application.Quit();
+    }
+}
